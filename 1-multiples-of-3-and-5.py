5c819dd1deeeab18f11bfb9c4bbd99a8fa7f6a26
Tim Swinkels <tim.swinkels@itility.nl>
2017-06-19 20:55:46 +0200

    1, multiles of 3 and 5

--- 1-multiples-of-3-and-5.py
+++ 1-multiples-of-3-and-5.py
@@ -1,11 +1,5 @@
-start = 0
-stop = 1000
-multiples_of_three = range(start, stop, 3)
-multiples_of_five = range(start, stop, 5)
+def getMultiplesOfUntil(multiples, limit):
+  answer = sum(x for x in range(limit) if (x % 3 == 0 or x % 5 == 0))
+  return answer
 
-multiples = multiples_of_three + multiples_of_five
-
-print(multiples)
-
-output = 'The sum of the multiples of 3 or 5 from 0 till 1000 is: %s'
-print(output % sum(multiples))
+print(getMultiplesOfUntil(4, 1000))
